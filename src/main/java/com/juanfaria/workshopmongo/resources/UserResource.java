659110e5b81654a3b08fbcd8bb99792561448ea4
Juan Faria <juanfariacrf@gmail.com>
2023-04-10 12:05:06 -0300

    Create insert users

--- src/main/java/com/juanfaria/workshopmongo/resources/UserResource.java
+++ src/main/java/com/juanfaria/workshopmongo/resources/UserResource.java
@@ -4,12 +4,12 @@ import com.juanfaria.workshopmongo.domain.User;
 import com.juanfaria.workshopmongo.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.bind.annotation.*;
 import com.juanfaria.workshopmongo.services.UserService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,5 +31,14 @@ public class UserResource {
 
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(new UserDto(obj));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDto userDto){
+        User obj = userService.fromDto(userDto);
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/juanfaria/workshopmongo/services/UserService.java
+++ src/main/java/com/juanfaria/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.juanfaria.workshopmongo.services;
 
 import com.juanfaria.workshopmongo.domain.User;
+import com.juanfaria.workshopmongo.dto.UserDto;
 import com.juanfaria.workshopmongo.repository.UserRepository;
 import com.juanfaria.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(()-> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj){
+        return userRepository.insert(obj);
+    }
+
+    public User fromDto(UserDto userDto){
+        return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
     }
 }
